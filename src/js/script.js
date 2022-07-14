91870de26bf32d2520bcbe3c5cd63d54a55aec81
smulczynskasia <107355316+smulczynskasia@users.noreply.github.com>
2022-07-14 20:36:32 +0200

    Add nwe class

--- src/js/script.js
+++ src/js/script.js
@@ -51,8 +51,24 @@
   const templates = {
     menuProduct: Handlebars.compile(document.querySelector(select.templateOf.menuProduct).innerHTML),
   };
+  
+  class Product {
+    constructor(){
+      const thisProduct = this;
+      
+      console.log{'new Product:', thisProduct);
+                 }
+    }
+    
 
   const app = {
+    
+    initMenu: function(){
+    const testProduct = new Product();
+    
+    console.log('testProduct:', testProduct);
+  }
+    
     init: function(){
       const thisApp = this;
       console.log('*** App starting ***');
@@ -60,8 +76,12 @@
       console.log('classNames:', classNames);
       console.log('settings:', settings);
       console.log('templates:', templates);
+       
+  thisApp.initMenu();
+      
     },
   };
-
+  
   app.init();
+ 
 }
