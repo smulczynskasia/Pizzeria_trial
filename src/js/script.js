d9da863917e7e965353c445bf32e389c9e3102ec
smulczynskasia <107355316+smulczynskasia@users.noreply.github.com>
2022-07-18 19:22:42 +0200

    Update script.js

--- src/js/script.js
+++ src/js/script.js
@@ -88,7 +88,7 @@
       const thisProduct = this;
       
     /* find the clickable trigger (the element that should react to clicking) */
-    const clickableTrigger = ???;
+    const clickableTrigger = thisProduct.element.querySelector(select.menuProduct.clickable);
 
     /* START: add event listener to clickable trigger on event click */
     clickableTrigger.addEventListener('click', function(event) {
