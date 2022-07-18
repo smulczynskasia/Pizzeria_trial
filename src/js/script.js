8e442a1a4ddc2d75ef9f05d1381524cf83e2b300
smulczynskasia <107355316+smulczynskasia@users.noreply.github.com>
2022-07-18 20:01:50 +0200

    Update script.js

--- src/js/script.js
+++ src/js/script.js
@@ -98,7 +98,7 @@
       
       /* find active product (product that has active class) */
 
-      const activeProduct = document.querySelectorAll(classNames.all.menuProduct.wrapperActive);
+      const activeProduct = document.querySelectorAll(select.all.menuProductsActive);
       
       /* if there is active product and it's not thisProduct.element, remove class active from it */
 
