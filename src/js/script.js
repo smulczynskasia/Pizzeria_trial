c67d187a02cbdcf5f592cc195941db555d12127f
smulczynskasia <107355316+smulczynskasia@users.noreply.github.com>
2022-07-19 21:30:51 +0200

    Update script.js

--- src/js/script.js
+++ src/js/script.js
@@ -130,10 +130,29 @@
     
     initOrderForm(){
       const thisProduct = this;
+      
+      thisProduct.form.addEventListener('submit', function(event){
+  event.preventDefault();
+  thisProduct.processOrder();
+});
+
+for(let input of thisProduct.formInputs){
+  input.addEventListener('change', function(){
+    thisProduct.processOrder();
+  });
+}
+
+thisProduct.cartButton.addEventListener('click', function(event){
+  event.preventDefault();
+  thisProduct.processOrder();
+});
     }
     
     processOrder(){
       const thisProduct = this;
+      
+      const formData = utils.serializeFormToObject(thisProduct.form);
+      console.log('formData:', formData);
     }
   }
     
