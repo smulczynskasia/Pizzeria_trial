723f485347dc19705ffceb84278e8f253c281eb4
smulczynskasia <107355316+smulczynskasia@users.noreply.github.com>
2022-08-14 14:04:16 +0200

    Update script.js

--- src/js/script.js
+++ src/js/script.js
@@ -195,7 +195,14 @@
       thisPrpduct.amountWidget = new AmountWidget(thisProduct.amountWidgetElem);
     }
     
- 
+    getElements(element){
+  const thisWidget = this;
+
+  thisWidget.element = element;
+  thisWidget.input = thisWidget.element.querySelector(select.widgets.amount.input);
+  thisWidget.linkDecrease = thisWidget.element.querySelector(select.widgets.amount.linkDecrease);
+  thisWidget.linkIncrease = thisWidget.element.querySelector(select.widgets.amount.linkIncrease);
+}
 
   }
   
