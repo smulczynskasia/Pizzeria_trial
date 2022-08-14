1e79957dc088cecd532940403ea8bd8c2a5e8e0e
smulczynskasia <107355316+smulczynskasia@users.noreply.github.com>
2022-08-14 16:34:30 +0200

    Update script.js

--- src/js/script.js
+++ src/js/script.js
@@ -31,6 +31,24 @@
         linkIncrease: 'a[href="#more"]',
       },
     },
+    cart: {
+    productList: '.cart__order-summary',
+    toggleTrigger: '.cart__summary',
+    totalNumber: `.cart__total-number`,
+    totalPrice: '.cart__total-price strong, .cart__order-total .cart__order-price-sum strong',
+    subtotalPrice: '.cart__order-subtotal .cart__order-price-sum strong',
+    deliveryFee: '.cart__order-delivery .cart__order-price-sum strong',
+    form: '.cart__order',
+    formSubmit: '.cart__order [type="submit"]',
+    phone: '[name="phone"]',
+    address: '[name="address"]',
+  },
+  cartProduct: {
+    amountWidget: '.widget-amount',
+    price: '.cart__product-price',
+    edit: '[href="#edit"]',
+    remove: '[href="#remove"]',
+  },
   };
 
   const classNames = {
@@ -38,19 +56,27 @@
       wrapperActive: 'active',
       imageVisible: 'active',
     },
+     cart: {
+    wrapperActive: 'active',
+  },
   };
 
   const settings = {
-    amountWidget: {
-      defaultValue: 1,
-      defaultMin: 1,
-      defaultMax: 9,
-    }
+  amountWidget: {
+    defaultValue: 1,
+    defaultMin: 1,
+    defaultMax: 9,
+  }, 
+  cart: {
+    defaultDeliveryFee: 20,
+  },
+};
     
   };
   console.log(settings);
   const templates = {
     menuProduct: Handlebars.compile(document.querySelector(select.templateOf.menuProduct).innerHTML),
+    cartProduct: Handlebars.compile(document.querySelector(select.templateOf.cartProduct).innerHTML),
   };
 
   class Product {
