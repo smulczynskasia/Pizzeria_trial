77ab0aa59123bc30a6fab670e7905f0f2cad622a
smulczynskasia <107355316+smulczynskasia@users.noreply.github.com>
2023-01-01 23:06:54 +0100

    Update AmountWidget.js

--- src/js/components/AmountWidget.js
+++ src/js/components/AmountWidget.js
@@ -1,51 +1,56 @@
-import { select, settings } from '../settings.js';
+import {settings, select} from './../settings.js';
 import BaseWidget from './BaseWidget.js';
 
 class AmountWidget extends BaseWidget {
   constructor(element) {
     super(element, settings.amountWidget.defaultValue);
+
     const thisWidget = this;
 
     thisWidget.getElements(element);
-    thisWidget.setValue(thisWidget.dom.input.value);
+
     thisWidget.initActions();
+
+    //console.log('AmountWidget:', thisWidget);
   }
 
-  getElements() {
+  getElements(){
     const thisWidget = this;
 
     thisWidget.dom.input = thisWidget.dom.wrapper.querySelector(select.widgets.amount.input);
-    thisWidget.dom.linkDecrease = thisWidget.dom.wrapper.querySelector(select.widgets.amount.linkDecrease);
+    thisWidget.dom.linkDecrease= thisWidget.dom.wrapper.querySelector(select.widgets.amount.linkDecrease);
     thisWidget.dom.linkIncrease = thisWidget.dom.wrapper.querySelector(select.widgets.amount.linkIncrease);
   }
 
-  isValid(value) {
+  isValid(value){
     return !isNaN(value)
-			&& value >= settings.amountWidget.defaultMin
-			&& value <= settings.amountWidget.defaultMax;
+    && value >= settings.amountWidget.defaultMin -1
+    && value <= settings.amountWidget.defaultMax +1;
   }
 
-  renderValue() {
+  renderValue(){
     const thisWidget = this;
+
     thisWidget.dom.input.value = thisWidget.value;
   }
 
-  initActions() {
+  initActions(){
     const thisWidget = this;
 
     thisWidget.dom.input.addEventListener('change', function() {
+      //thisWidget.setValue(thisWidget.dom.input.value);
       thisWidget.value = thisWidget.dom.input.value;
     });
-
     thisWidget.dom.linkDecrease.addEventListener('click', function(event) {
       event.preventDefault();
       thisWidget.setValue(thisWidget.value - 1);
     });
-
     thisWidget.dom.linkIncrease.addEventListener('click', function(event) {
       event.preventDefault();
       thisWidget.setValue(thisWidget.value + 1);
     });
   }
+
 }
+
 export default AmountWidget;
